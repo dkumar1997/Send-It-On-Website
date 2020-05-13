87a66b2520710a1c568e0aa2e99bf9b2fdbf3d10
Dheeraj Kumar <dheeraj.kumar@ucalgary.ca>
2020-05-12 22:05:44 -0600

    minor adjustments along with about the family section finished needs a few more to make it look bit nicer

---
+++ js_files/aboutfamily.js
@@ -0,0 +1,19 @@
+const sliders = document.querySelectorAll(".slide");
+const appearOptions = {
+  threshold:0,
+  rootMargin: "0px 0px -300px 0px"
+};
+const appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll){
+  entries.forEach(entry => {
+    if(!entry.isIntersecting){
+      return;
+    }
+    else{
+      entry.target.classList.add("appear");
+      appearOnScroll.unobserve(entry.target);
+    }
+  })
+}, appearOptions);
+sliders.forEach(slider => {
+  appearOnScroll.observe(slider);
+});
