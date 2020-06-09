72972113c4f99e46a35e520ba12de06e531392dc
Dheeraj Kumar <dheeraj.kumar@ucalgary.ca>
2020-06-08 21:02:11 -0600

    updated header with proper nav bar for phone

--- js_files/main_page.js
+++ js_files/main_page.js
@@ -1,27 +1,15 @@
-const bar= document.querySelector(".nav-bar");
-const sectionOne = document.querySelector("header");
 
-const sectionOneOptions = {
-  rootMargin: "-100px 0px 0px 0px"
-};
 
-const sectionOneObserver = new IntersectionObserver(function(
-  entries,
-  sectionOneObserver
-) {
-  entries.forEach(entry => {
-    if (!entry.isIntersecting) {
-      bar.classList.add("nav-bar-color");
-						bar.style.top = "0";
-    } else {
-      bar.classList.remove("nav-bar-color");
-					 bar.style.top = "20px";
-						
-    }
-  });
-},
-sectionOneOptions);
 
-sectionOneObserver.observe(sectionOne);
+
+$(document).ready(function(){
+  $('.menu-toggle').click(function(){
+    $('nav').toggleClass('active');
+  })
+  $('ul li').click(function(){
+    $(this).siblings().removeClass('active');
+    $(this).toggleClass('active');
+  })
+})
 
 
